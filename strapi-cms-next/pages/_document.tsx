982c70b4f8f9072056b332eb0fec71b5b47e38bd
npham49 <brian.d.pham@gov.bc.ca>
2023-01-05 16:47:47 -0800

    added bcgov bootstrap in NextJS

--- strapi-cms-next/pages/_document.tsx
+++ strapi-cms-next/pages/_document.tsx
@@ -3,7 +3,24 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link
+        href="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossOrigin="anonymous"
+      ></link>
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossOrigin="anonymous"
+      ></link>
+      <link
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossOrigin="anonymous"
+      ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
